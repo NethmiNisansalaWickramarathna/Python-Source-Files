ca5b0286501edc49144e0c7de4531e9967b971f5
NethmiNisansalaWickramarathna <nisansalanethmi58@gmail.com>
2025-06-01 13:27:36 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,8 @@
+for i in range (1,3):
+  for j in range (1,3):
+    for k in range (1,3):
+      if i==j==k:
+        print(f"i={i},j={j},k={k}NETHU")
+      else:
+        print(f"i={i},j={j},k={k} NETHMI")
+      
